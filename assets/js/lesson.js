37796ecdd2e321deb8b153b7fcd7bc073668bea4
Greg Wilson <gvwilson@third-bit.com>
2016-04-14 21:10:40 -0400

    Generic files

---
+++ assets/js/lesson.js
@@ -0,0 +1,10 @@
+// Make all tables striped by default.
+$("table").addClass("table table-striped");
+
+// Handle searches.
+// Relies on document having 'meta' element with name 'search-domain'.
+function google_search() {
+  var query = document.getElementById("google-search").value;
+  var domain = $("meta[name=search-domain]").attr("value");
+  window.open("https://www.google.com/search?q=" + query + "+site:" + domain);
+}
