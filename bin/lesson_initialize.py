ac87861eb0104eead5ab12b3101a4c5d43231e87
Greg Wilson <gvwilson@third-bit.com>
2016-06-16 08:07:10 -0400

    Fixing mailing list link in CONTRIBUTING

--- bin/lesson_initialize.py
+++ bin/lesson_initialize.py
@@ -148,7 +148,7 @@ You can also [reach us by email][contact].
 [dc-issues]: https://github.com/issues?q=user%3Adatacarpentry
 [dc-lessons]: http://datacarpentry.org/lessons/
 [dc-site]: http://datacarpentry.org/
-[discuss-list]: http://lists.software-carpentry.org/mailman/listinfo/discuss_lists.software-carpentry.org
+[discuss-list]: http://lists.software-carpentry.org/listinfo/discuss
 [example-site]: https://swcarpentry.github.io/lesson-example/
 [github]: http://github.com
 [github-flow]: https://guides.github.com/introduction/flow/
